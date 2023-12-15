b67c81869e00b0ef4d15c74ccbbcad1f89480607
Ye Bhone Lin <106800189+Ye-Bhone-Lin@users.noreply.github.com>
2023-12-15 17:57:12 +0630

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import cv2
 import streamlit as st
 from io import BytesIO
 import time
+import logging
+logging.basicConfig(level=logging.DEBUG)
 load_model = tf.keras.models.load_model('custom_model')
 load_model2 = tf.keras.models.load_model('mobilenetv2_model')
 uploaded_file = st.file_uploader('Choose a car image', type=["jpg", "png", "jpeg"])
